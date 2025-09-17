4ebfe4f999b8bf6e33529e2746bf3fb933295d34
Frazer Dearlove <frizfraz21@gmail.com>
2025-09-17 17:18:58 +0800

    Wwise un-commit

--- Assets/Scripts/ManagerScripts/AudioManager.cs
+++ Assets/Scripts/ManagerScripts/AudioManager.cs
@@ -5,6 +5,7 @@ public class AudioManager : MonoBehaviour
     public static AudioManager instance;
 
 
+    /*
     public void PlayEvent(string eventName, GameObject emitter = null)
     {
         if (emitter == null) { emitter = gameObject; }
@@ -16,4 +17,5 @@ public class AudioManager : MonoBehaviour
         if (emitter == null) { emitter = gameObject; }
         AkUnitySoundEngine.ExecuteActionOnEvent(eventName, AkActionOnEventType.AkActionOnEventType_Stop, emitter);
     }
+    */
 }
