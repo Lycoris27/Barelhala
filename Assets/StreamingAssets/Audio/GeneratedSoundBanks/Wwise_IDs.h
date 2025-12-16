7efce5d28045c27a0c4084b217005136471378a9
squeakyvermin <cody@bloomaudio.work>
2025-12-17 04:16:36 +0800

    Audio Sound Design Assets & Events
    Have added in the audio assets & the events tied to them.
    
    - Character Hit (Grunts)
    - Character Hit (w/o Grunts)
    - Enemy hits (mobs)
    - Footsteps
    - Machine Guns
    - SciFi & Laser Guns
    - Spell Attacks
    - Bombs / Explosion SFX
    - UI SFX
    
    Bouncing Music to push ASAP now.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -25,7 +25,15 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID BOOM = 1761501016U;
+        static const AkUniqueID CHARACTER = 436743010U;
+        static const AkUniqueID COMBAT = 2764240573U;
+        static const AkUniqueID ENEMIES = 2242381963U;
+        static const AkUniqueID GUNS___AMMO = 1520494568U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MX = 1685527054U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
